e7019f8e87be0985e8ada5c874654dafd18ddb8f
Owen Diehl <ow.diehl@gmail.com>
2019-05-20 22:41:05 -0400

    init

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for doemane
+
+## Unreleased changes
---
+++ src/PTree.hs
@@ -0,0 +1,26 @@
+module PTree where
+
+import           Data.Map        (Map)
+import qualified Data.Map.Strict as Map
+
+data PTree a = Empty | Node (Map a (PTree a))
+  deriving (Show)
+
+-- fromWords :: [[a]] -> PTree a
+-- fromWords words =
+
+insert :: Ord a => PTree a -> [a] -> PTree a
+insert tree []  = tree
+insert tree tokens =
+  case tree of
+    Empty         -> Node (newChild tokens)
+    Node children -> Node (update tokens children)
+  where
+    newChild []     = Map.empty
+    newChild (x:xs) = Map.singleton x (insert Empty xs)
+    update [] children = children
+    update (x:xs) children =
+      Map.alter updater x children where
+      updater found = case found of
+        Nothing -> Just $ insert Empty xs
+        Just a  -> Just $ insert a xs
