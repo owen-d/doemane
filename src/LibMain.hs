b8be5663dfd1636f986da6b47b69d36a51f08396
Owen Diehl <ow.diehl@gmail.com>
2019-05-21 17:36:35 -0400

    db helpers

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -1,21 +1,39 @@
 module LibMain where
 
-import           CLI        (Config (dictionary), runConfig)
-import           Data.Maybe (catMaybes)
-import           Vocab      (parseInput)
-
+import           CLI             (Config (dictionary), runConfig)
+import           Data.Map        (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Maybe      (catMaybes)
+import           Data.Tuple      (swap)
+import           PTree           (PTree, fromList)
+import           Vocab           (parseInput)
 
 main :: IO ()
 main = runConfig run
 
+wordCache :: (Ord k, Eq k, Eq v) => [(v, k)] -> Map k v
+wordCache pairs =
+  Map.fromList $ map swap pairs
+
+-- builds a wordCache and PTree from a parsed list of (sounds,word) pairs
+buildDatabases :: (Ord k, Eq k, Ord v, Eq v) => [([k], v)] -> (Map v [k], PTree k v)
+buildDatabases pairs =
+  (wordCache pairs, fromList pairs)
+
+buildPairs :: (Ord a, Eq a) => [[a]] -> [([a], a)]
+buildPairs lines =
+  catMaybes $ map mapper lines
+  where
+    mapper []            = Nothing
+    mapper (word:sounds) = Just (sounds,word)
+
 run :: Config -> IO ()
 run conf = do
   parsed <- parseInput . dictionary $ conf
-  print $ fmap fn parsed
+  case parsed of
+    Left e        -> print e
+    Right parsed' -> fn parsed'
   where
-    -- fn = take 5
-    fn lines =
-      let fmt = catMaybes $ map mapper lines
-      in take 5 fmt
-    mapper []            = Nothing
-    mapper (word:sounds) = Just (sounds,word)
+    fn lines = do
+      let (cache, _) = buildDatabases $ buildPairs $ take 5 lines
+      print cache
