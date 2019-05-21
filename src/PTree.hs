1006720830fbfd1d1d094fac8ce00f57bf46c740
Owen Diehl <ow.diehl@gmail.com>
2019-05-21 14:14:32 -0400

    more idiomatic Trie nodes. No need for an explicit empty

--- src/PTree.hs
+++ src/PTree.hs
@@ -1,28 +1,33 @@
 module PTree where
 
+import           Data.List       (foldl')
 import           Data.Map        (Map)
 import qualified Data.Map.Strict as Map
 
-data PTree a = Empty | Node (Map a (PTree a))
-  deriving (Show)
+data PTree a =
+  Node
+    { children :: Map a (PTree a)
+    }
+  deriving (Show, Eq)
 
-fromList :: Ord a => [[a]] -> PTree a
-fromList [] = Empty
+empty = Node Map.empty
+
+fromList :: (Ord a, Eq a) => [[a]] -> PTree a
+fromList [] = empty
 fromList xs =
-  foldl insert Empty xs
+  foldl' insert empty xs
 
-insert :: Ord a => PTree a -> [a] -> PTree a
+insert :: (Ord a, Eq a) => PTree a -> [a] -> PTree a
 insert tree []  = tree
-insert tree tokens =
-  case tree of
-    Empty         -> Node (newChild tokens)
-    Node children -> Node (update tokens children)
+insert tree tokens
+  | (children tree) == Map.empty = Node $ newChild tokens
+  | otherwise = Node $ update tokens $ children tree
   where
     newChild []     = Map.empty
-    newChild (x:xs) = Map.singleton x (insert Empty xs)
+    newChild (x:xs) = Map.singleton x (insert empty xs)
     update [] children = children
-    update (x:xs) children =
-      Map.alter updater x children where
+    update (x:xs) childMap =
+      Map.alter updater x childMap where
       updater found = case found of
-        Nothing -> Just $ insert Empty xs
+        Nothing -> Just $ insert empty xs
         Just a  -> Just $ insert a xs
