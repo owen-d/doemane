5c422be7da9743e54878c6de41b9d6bcce573cd3
Owen Diehl <ow.diehl@gmail.com>
2019-05-30 16:38:38 -0400

    adds 3000 common vocab words

--- src/Vocab.hs
+++ src/Vocab.hs
@@ -1,8 +1,10 @@
 module Vocab
   ( Vocab.Parser.parseInput
   , Symbols.Symbol(..)
+  , Vocab.Common3000.common
   ) where
 
+import qualified Vocab.Common3000
 import qualified Vocab.Parser
-import qualified Vocab.Symbols as Symbols
+import qualified Vocab.Symbols    as Symbols
 
